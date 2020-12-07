535550ce7b9f52447af3d363fae0afb66c003a3f
Eduardo Henrique <eduardohs29@gmail.com>
2020-12-07 19:55:11 -0300

    Atualização 07/12/2020

--- src/ClassePrincipal/Main.java
+++ src/ClassePrincipal/Main.java
@@ -1,5 +1,11 @@
 package ClassePrincipal;
+import View.SingIn;
 public class Main {
     public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new SingIn().setVisible(true);
+            }
+        });
     }
 }
